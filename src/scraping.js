3fbd88013c41571c6998fdfad94e2376f70a20f4
Killian Courvoisier <killian.courvoisier@cozycloud.cc>
2023-02-27 16:43:31 +0100

    lint scraping.js

--- src/scraping.js
+++ src/scraping.js
@@ -1,5 +1,5 @@
-import {parse} from 'date-fns'
-import {fr} from 'date-fns/locale'
+import { parse } from 'date-fns'
+import { fr } from 'date-fns/locale'
 import Minilog from '@cozy/minilog'
 
 const log = Minilog('scraping')
@@ -7,33 +7,33 @@ const baseUrl = 'https://www.amazon.fr'
 
 const commandParser = {
   shipmentMessage: {
-    sel: '.a-color-success',
+    sel: '.a-color-success'
   },
   amount: {
     sel: '.order-info .a-fixed-right-grid-inner > .a-col-left > .a-row > div:nth-child(2) .value',
-    parse: parseAmount,
+    parse: parseAmount
   },
   currency: {
     sel: '.order-info .a-fixed-right-grid-inner > .a-col-left > .a-row > div:nth-child(2) .value',
-    parse: parseCurrency,
+    parse: parseCurrency
   },
   commandDate: {
     sel: '.order-info .a-fixed-right-grid-inner > .a-col-left > .a-row > div:nth-child(1) .value',
-    parse: parseCommandDate,
+    parse: parseCommandDate
   },
   vendorRef: {
     sel: "a[href*='order-details'],a[href*='order-summary']",
     attr: 'href',
-    parse: (href) => {
+    parse: href => {
       if (!href) {
         return false
       }
       return new URL(baseUrl + href).searchParams.get('orderID')
-    },
+    }
   },
   detailsUrl: {
     sel: "a[href*='order-details'],a[href*='order-summary']",
-    fn: ($) => {
+    fn: $ => {
       const json = $.closest('ul')
         .find('[data-a-popover]')
         .attr('data-a-popover')
@@ -43,11 +43,11 @@ const commandParser = {
         log.warn(err.message)
         return false
       }
-    },
-  },
+    }
+  }
 }
 
-export const parseCommands = (resp) => resp.scrape(commandParser, '.order')
+export const parseCommands = resp => resp.scrape(commandParser, '.order')
 
 function parseAmount(amount) {
   return parseFloat(amount.split(' ').pop().replace(',', '.'))
@@ -58,6 +58,6 @@ function parseCurrency(amount) {
 }
 
 function parseCommandDate(date) {
-  const result = parse(date, 'd MMMM u', new Date(), {locale: fr})
+  const result = parse(date, 'd MMMM u', new Date(), { locale: fr })
   return result
 }
